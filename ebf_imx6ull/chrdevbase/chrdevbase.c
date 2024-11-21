9ff8733febf1ed7fdee1ddfb2067b29525fe0316
celeron633 <dengxionghui@hotmail.com>
2024-11-21 23:19:58 +0800

    chrdevbase: refind code

--- ebf_imx6ull/chrdevbase/chrdevbase.c
+++ ebf_imx6ull/chrdevbase/chrdevbase.c
@@ -10,12 +10,53 @@
 #define CHRDEVBASE_MAJOR 200
 #define CHRDEVBASE_NAME "chrdevbase"
 
-static int chrdevbase_init(void);
-static void chrdevbase_exit(void);
-static int chrdevbase_open(struct inode *, struct file *);
-static int chrdevbase_release(struct inode *, struct file *);
-static ssize_t chrdevbase_read(struct file *f, char __user *buf, size_t len, loff_t *off);
-static ssize_t chrdevbase_write(struct file *f, const char __user *buf, size_t len, loff_t *off);
+static int chrdevbase_open(struct inode *inodep, struct file *filep)
+{
+    printk(KERN_NOTICE "open\r\n");
+
+    return 0;
+}
+
+static int chrdevbase_release(struct inode *inodep, struct file *filep)
+{
+    printk(KERN_NOTICE "release\r\n");
+
+    return 0;
+}
+
+static ssize_t chrdevbase_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+{
+    char msg_to_user[] = "hello wrorld\r\n";
+    int ret;
+
+    printk(KERN_NOTICE "read\r\n");
+    ret = copy_to_user(buf, msg_to_user, sizeof(msg_to_user));
+    if (ret < 0) {
+        printk(KERN_ERR "copy_to_user failed!\r\n");
+        return EFAULT;
+    }
+    
+    return sizeof(msg_to_user);
+}
+
+static ssize_t chrdevbase_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
+{
+    char buffer[512];
+    int ret;
+
+    printk(KERN_NOTICE "write\r\n");
+    if (len > 512) {
+        len = 512;
+    }
+    ret = copy_from_user(buffer, buf, len);
+    if (ret < 0) {
+        printk(KERN_ERR "copy_from_user failed!\r\n");
+        return EFAULT;
+    }
+    printk(KERN_NOTICE "read %d bytes from userspace: [%s]\r\n", (int)len, buffer);
+
+    return len;
+}
 
 struct file_operations chrdevbase_fops = {
     .open = chrdevbase_open,
@@ -29,7 +70,7 @@ static int chrdevbase_init(void)
     printk(KERN_NOTICE "init");
 
     if (register_chrdev(CHRDEVBASE_MAJOR, CHRDEVBASE_NAME, &chrdevbase_fops) < 0) {
-        printk(KERN_ERR "register_chrdev failed");
+        printk(KERN_ERR "register_chrdev failed\r\n");
         return EFAULT;
     }
 
@@ -38,56 +79,9 @@ static int chrdevbase_init(void)
 
 static void chrdevbase_exit(void)
 {
-    printk(KERN_NOTICE "exit");
+    printk(KERN_NOTICE "exit\r\n");
 
     unregister_chrdev(CHRDEVBASE_MAJOR, CHRDEVBASE_NAME);
-}
-
-static int chrdevbase_open(struct inode *inodep, struct file *filep)
-{
-    printk(KERN_NOTICE "open");
-
-    return 0;
-}
-
-static int chrdevbase_release(struct inode *inodep, struct file *filep)
-{
-    printk(KERN_NOTICE "release");
-
-    return 0;
-}
-
-static ssize_t chrdevbase_read(struct file *f, char __user *buf, size_t len, loff_t *off)
-{
-    char msg_to_user[] = "hello wrorld\n";
-    int ret;
-
-    printk(KERN_NOTICE "read");
-
-    ret = copy_to_user(buf, msg_to_user, sizeof(msg_to_user));
-    if (ret < 0) {
-        printk(KERN_ERR "copy_to_user failed!");
-        return EFAULT;
-    }
-    
-    return sizeof(msg_to_user);
-}
-
-static ssize_t chrdevbase_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
-{
-    char buffer[2048];
-    int ret;
-
-    printk(KERN_NOTICE "write");
-
-    ret = copy_from_user(buffer, buf, len);
-    if (ret < 0) {
-        printk(KERN_ERR "copy_from_user failed!");
-        return EFAULT;
-    }
-    printk(KERN_NOTICE "read %d bytes from userspace: [%s]", (int)len, buffer);
-
-    return len;
 }
 
 MODULE_LICENSE("GPL");
