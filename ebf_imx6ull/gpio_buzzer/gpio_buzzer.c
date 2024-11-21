97b81e1d403b3aabe2230180056ef75c45313be9
celeron633 <dengxionghui@hotmail.com>
2024-11-21 23:30:33 +0800

    gpio_buzzer: finally working, miscify & refine 1. bug cause: call misc_deregister in platform probe! typo

--- ebf_imx6ull/gpio_buzzer/gpio_buzzer.c
+++ ebf_imx6ull/gpio_buzzer/gpio_buzzer.c
@@ -15,30 +15,10 @@
 #include <linux/atomic.h>
 #include <linux/miscdevice.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("dengxh");
-
-#define BUZZER_MINOR 185
-
-// platform driver
-int platform_driver_buzzer_probe(struct platform_device *pdev);
-int platform_driver_buzzer_remove(struct platform_device *ppdev);
-
-// cdev && class && device
-static dev_t buzzer_dev_id;
-static struct cdev buzzer_cdev;
-static struct class *buzzer_class;
-static struct device *buzzer_device;
-
 // buzzer gpio
 int buzzer_gpio;
 
-// file operations
-static int buzzer_dev_open(struct inode *inodep, struct file *filep);
-static int buzzer_dev_release(struct inode *inodep, struct file *filep);
-static ssize_t buzzer_dev_read(struct file *f, char __user *buf, size_t len, loff_t *off);
-static ssize_t buzzer_dev_write(struct file *fp, const char __user *buf, size_t len, loff_t *off);
-
+// match table for dt
 struct of_device_id match_table[] = {
     {
         .compatible = "ebf,buzzer"
@@ -46,91 +26,6 @@ struct of_device_id match_table[] = {
     {    
     }
 };
-
-struct platform_driver platform_driver_buzzer = {
-    .probe = platform_driver_buzzer_probe,
-    .remove = platform_driver_buzzer_remove,
-    .driver = {
-        .name = "ebf-buzzer",
-        .of_match_table = match_table
-    }
-};
-
-struct file_operations buzzer_dev_fops = {
-    .owner = THIS_MODULE,
-    .open = buzzer_dev_open,
-    .release = buzzer_dev_release,
-    .read = buzzer_dev_read,
-    .write = buzzer_dev_write,
-};
-
-// static struct miscdevice buzzer_misc = {
-//     .fops = &buzzer_dev_fops,
-//     .minor = MISC_DYNAMIC_MINOR,
-//     .name = "ebf_buzzer"
-// };
-
-int platform_driver_buzzer_probe(struct platform_device *pdev)
-{
-    struct device_node *buzzer_node;
-
-    printk(KERN_NOTICE "platform_driver_buzzer_probe!\r\n");
-    // printk(KERN_NOTICE "misc_deregister begin!\r\n");
-    // misc_deregister(&buzzer_misc);
-    // printk(KERN_NOTICE "misc_deregister end!\r\n");
-
-    buzzer_node = of_find_node_by_path("/ebf_buzzer");
-    if (buzzer_node == NULL) {
-        printk(KERN_ERR "find node 'buzzer_node' failed\r\n");
-        return -EINVAL;
-    }
-    buzzer_gpio = of_get_named_gpio(buzzer_node, "buzzer-gpio", 0);
-    if (buzzer_gpio < 0) {
-        printk(KERN_ERR "of_get_named_gpio failed\r\n");
-        return -EINVAL;
-    }
-
-    gpio_direction_output(buzzer_gpio, 0);
-    gpio_set_value(buzzer_gpio, 0);
-
-    alloc_chrdev_region(&buzzer_dev_id, 0, 1, "ebf_buzzer");
-    cdev_init(&buzzer_cdev, &buzzer_dev_fops);
-    cdev_add(&buzzer_cdev, buzzer_dev_id, 1);
-    buzzer_class = class_create(THIS_MODULE, "ebf_buzzer");
-    buzzer_device = device_create(buzzer_class, NULL, buzzer_dev_id, NULL, "ebf_buzzer");
-
-    return 0;
-}
-
-int platform_driver_buzzer_remove(struct platform_device *ppdev)
-{
-    printk(KERN_NOTICE "platform_driver_buzzer_remove!\r\n");
-    // misc_deregister(&buzzer_misc);
-
-    gpio_free(buzzer_gpio);
-
-    cdev_del(&buzzer_cdev);
-    device_destroy(buzzer_class, buzzer_dev_id);
-    class_destroy(buzzer_class);
-    unregister_chrdev_region(buzzer_dev_id, 1);
-
-    return 0;
-}
-
-int platform_driver_buzzer_init(void)
-{
-    printk(KERN_NOTICE "platform_driver_buzzer_init\r\n");
-    platform_driver_register(&platform_driver_buzzer);
-
-    return 0;
-}
-
-void platform_driver_buzzer_exit(void)
-{
-    printk(KERN_NOTICE "platform_driver_buzzer_exit\r\n");
-
-    platform_driver_unregister(&platform_driver_buzzer);
-}
 
 static int buzzer_dev_open(struct inode *inodep, struct file *filep)
 {
@@ -148,7 +43,7 @@ static int buzzer_dev_release(struct inode *inodep, struct file *filep)
 
 static ssize_t buzzer_dev_read(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
-    // int ret;
+    printk(KERN_NOTICE "read\r\n");
     
     return 0;
 }
@@ -159,6 +54,9 @@ static ssize_t buzzer_dev_write(struct file *fp, const char __user *buf, size_t 
     int ret;
     printk(KERN_NOTICE "write\r\n");
 
+    if (len > 16) {
+        len = 16;
+    }
     ret = copy_from_user(buffer, buf, len);
     if (ret < 0) {
         printk(KERN_ERR "copy_from_user failed!\r\n");
@@ -175,6 +73,82 @@ static ssize_t buzzer_dev_write(struct file *fp, const char __user *buf, size_t 
     return len;
 }
 
+struct file_operations buzzer_dev_fops = {
+    .owner = THIS_MODULE,
+    .open = buzzer_dev_open,
+    .release = buzzer_dev_release,
+    .read = buzzer_dev_read,
+    .write = buzzer_dev_write,
+};
 
+static struct miscdevice buzzer_misc = {
+    .fops = &buzzer_dev_fops,
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "gpio_buzzer"
+};
+
+int platform_driver_buzzer_probe(struct platform_device *pdev)
+{
+    struct device_node *buzzer_node;
+
+    printk(KERN_NOTICE "platform_driver_buzzer_probe!\r\n");
+
+    buzzer_node = of_find_node_by_path("/ebf_buzzer");
+    if (buzzer_node == NULL) {
+        printk(KERN_ERR "find node 'buzzer_node' failed\r\n");
+        return -EINVAL;
+    }
+    buzzer_gpio = of_get_named_gpio(buzzer_node, "buzzer-gpio", 0);
+    if (buzzer_gpio < 0) {
+        printk(KERN_ERR "of_get_named_gpio failed\r\n");
+        return -EINVAL;
+    }
+
+    gpio_direction_output(buzzer_gpio, 0);
+    gpio_set_value(buzzer_gpio, 0);
+
+    printk(KERN_NOTICE "misc_register begin!\r\n");
+    misc_register(&buzzer_misc);
+    printk(KERN_NOTICE "misc_register end!\r\n");
+
+    return 0;
+}
+
+int platform_driver_buzzer_remove(struct platform_device *ppdev)
+{
+    printk(KERN_NOTICE "platform_driver_buzzer_remove!\r\n");
+
+    gpio_free(buzzer_gpio);
+    misc_deregister(&buzzer_misc);
+
+    return 0;
+}
+
+struct platform_driver platform_driver_buzzer = {
+    .probe = platform_driver_buzzer_probe,
+    .remove = platform_driver_buzzer_remove,
+    .driver = {
+        .name = "ebf-buzzer",
+        .of_match_table = match_table
+    }
+};
+
+int __init platform_driver_buzzer_init(void)
+{
+    printk(KERN_NOTICE "platform_driver_buzzer_init\r\n");
+    platform_driver_register(&platform_driver_buzzer);
+
+    return 0;
+}
+
+void __exit platform_driver_buzzer_exit(void)
+{
+    printk(KERN_NOTICE "platform_driver_buzzer_exit\r\n");
+
+    platform_driver_unregister(&platform_driver_buzzer);
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("dengxh");
 module_init(platform_driver_buzzer_init);
 module_exit(platform_driver_buzzer_exit);
